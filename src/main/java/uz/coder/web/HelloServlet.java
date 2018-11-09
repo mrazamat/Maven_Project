883adbaa87ef540d86e9157639b1e1daedd37180
mrazamat <azamat00189@mail.ru>
2018-11-09 18:50:09 +0500

    Аннотация WebServlet

--- src/main/java/uz/coder/web/HelloServlet.java
+++ src/main/java/uz/coder/web/HelloServlet.java
@@ -1,15 +1,17 @@
 package uz.coder.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/hi")
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         PrintWriter printWriter = resp.getWriter();
         printWriter.write("Hello world. It is from HelloServlet ;)");
     }
